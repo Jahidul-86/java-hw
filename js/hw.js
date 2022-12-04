86e31a58531ece7fc63448526fb93658173570fe
Jahidul-86 <115869487+Jahidul-86@users.noreply.github.com>
2022-12-04 19:35:47 +0600

    Add files via upload

---
+++ js/hw.js
@@ -0,0 +1,32 @@
+// Odd/Even Number Checkup
+
+let Number = 5
+
+let remainder = (Number % 2)
+if (remainder == 0)
+{
+    console .log (`${Number} is even`)
+}else{
+    console .log (`${Number} is odd`)
+}
+
+
+// Mathematical Operation with function
+
+let x = 3.14;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 2);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+//maximum = Math.max(x, y, z);
+// minimum = Math.min(x, y, z);
+// x = Math.PI;
+
+console.log(x);
